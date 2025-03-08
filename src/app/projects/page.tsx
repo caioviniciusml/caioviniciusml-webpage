be4cc0d3e6a8be14e93534e74a0b8032d2d5e5e8
Caio Vinicius <devcaioviniciusml@gmail.com>
2025-03-07 23:30:06 -0300

    feat: projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,11 @@
 export default function Projects() {
   return (
-    <div>
-      Projects
-    </div>
+    <main className="grid grid-cols-1 gap-5">
+      <h1 className="text-white text-4xl font-bold">Projects</h1>
+      <p className="text-justify text-grayprimary text-base lg:text-xl">This section showcases my personal projects, highlighting my expertise and skills developing web applications using modern technologies such as React, Next.js, TypeScript, Node.js, Express, and More.</p>
+      <div className="py-10">
+        
+      </div>
+    </main>
   );
 }
