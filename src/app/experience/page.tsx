0cfe4e2d6e710147eaa71eaa1d5b3dbd95b4e79b
Caio Vinicius <devcaioviniciusml@gmail.com>
2025-03-08 06:24:15 -0300

    feat: work experiences page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,7 +1,8 @@
-export default function Experience() {
+export default function Projects() {
   return (
-    <div>
-      Experience
-    </div>
+    <main className="grid grid-cols-1 gap-5">
+      <h1 className="text-white text-4xl font-bold">Work Experience</h1>
+      <p className="text-justify text-grayprimary text-base lg:text-xl">This section presents my work experience, detailing the companies I have worked with, my roles, key responsibilities and contributions. It showcases my abilities developing scalable web solutions, collaborating with teams and applying modern technologies to meet business objectives.</p>
+    </main>
   );
 }
