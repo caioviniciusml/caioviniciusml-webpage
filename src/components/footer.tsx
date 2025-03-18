089f8b44fb1702509929d73ed1639c33cddd8fac
Caio Vinicius <devcaioviniciusml@gmail.com>
2025-03-18 20:52:25 -0300

    refactor: adding brand to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export function Footer() {
     return (
-        <footer className="mt-10 sm:mt-20 p-5 sm:p-10 w-full max-w-screen-xl grid grid-cols-1 gap-2 text-[10px] sm:text-sm text-graysecondary">
-            <span>caioviniciusml</span>
+        <footer className="mt-10 md:mt-20 p-5 sm:p-10 w-full max-w-screen-xl flex flex-col gap-2 md:gap-5 text-[10px] sm:text-sm text-graysecondary">
+            <Image
+                className="w-32 sm:w-40 opacity-50"
+                src={'/brand.png'}
+                width={750}
+                height={146}
+                alt="Caioviniciusml Brand">
+            </Image>
             <div className="flex items-center justify-between">
                 <div className="grid grid-cols-1 gap-1 sm:gap-2">
                     <span>Full Stack Software Engineer</span>
